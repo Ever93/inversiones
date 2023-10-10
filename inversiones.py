76371d3f277870d4aeec7ea1e251490436f44f31
Ever <informaticoebz@gmail.com>
2023-10-09 21:21:55 -0300

    Prototipo terminado

--- inversiones.py
+++ inversiones.py
@@ -1,12 +1,31 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QDialog, QVBoxLayout
+
+class MontoInputDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        layout = QVBoxLayout()
+
+        self.monto_label = QtWidgets.QLabel("Monto:")
+        self.monto_input = QtWidgets.QLineEdit()
+        self.ok_button = QtWidgets.QPushButton("OK")
+
+        layout.addWidget(self.monto_label)
+        layout.addWidget(self.monto_input)
+        layout.addWidget(self.ok_button)
+
+        self.ok_button.clicked.connect(self.accept)
+
+        self.setLayout(layout)
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setObjectName("MainWindow")
-        self.resize(833, 640)
+        self.resize(833, 654)
         
         self.centralwidget = QtWidgets.QWidget(self)
         self.centralwidget.setObjectName("centralwidget")
@@ -55,11 +74,12 @@ class MainWindow(QtWidgets.QMainWindow):
         font.setBold(True)
         font.setWeight(75)
         self.label_4.setFont(font)
-        self.label_4.setObjectName("label_4")
+        self.label_4.setObjectName("fechaSaldo")
         
         self.cargaCI = QtWidgets.QPushButton(self.centralwidget)
         self.cargaCI.setGeometry(QtCore.QRect(20, 70, 75, 23))
         self.cargaCI.setObjectName("cargaCI")
+        self.cargaCI.clicked.connect(self.abrir_dialogo)
         
         self.ingreso = QtWidgets.QPushButton(self.centralwidget)
         self.ingreso.setGeometry(QtCore.QRect(150, 160, 75, 23))
@@ -73,9 +93,46 @@ class MainWindow(QtWidgets.QMainWindow):
         self.treeViewIngreso.setGeometry(QtCore.QRect(10, 200, 391, 391))
         self.treeViewIngreso.setObjectName("treeViewIngreso")
         
+        # Crear el modelo de datos para el QTreeView
+        self.modelIngreso = QtGui.QStandardItemModel()
+        self.treeViewIngreso.setModel(self.modelIngreso)
+        # Agregar encabezados de columna
+        self.modelIngreso.setHorizontalHeaderLabels(["Fecha", "Detalle", "Monto"])
+        self.treeViewIngreso.setColumnWidth(1, 200)
+        
         self.treeViewEgreso = QtWidgets.QTreeView(self.centralwidget)
         self.treeViewEgreso.setGeometry(QtCore.QRect(430, 200, 391, 391))
         self.treeViewEgreso.setObjectName("treeViewEgreso")
+        # Crear el modelo de datos para el QTreeView
+        self.modelEgreso = QtGui.QStandardItemModel()
+        self.treeViewEgreso.setModel(self.modelEgreso)
+        # Agregar encabezados de columna
+        self.modelEgreso.setHorizontalHeaderLabels(["Fecha", "Detalle", "Monto"])
+        self.treeViewEgreso.setColumnWidth(1, 200)
+        self.textCI = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textCI.setGeometry(QtCore.QRect(140, 50, 211, 31))
+        self.textCI.setObjectName("textCI")
+        self.textSaldo = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textSaldo.setGeometry(QtCore.QRect(70, 110, 211, 31))
+        self.textSaldo.setObjectName("textSaldo")
+        self.textFechaCI = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textFechaCI.setGeometry(QtCore.QRect(580, 50, 211, 31))
+        self.textFechaCI.setObjectName("textFechaCI")
+        self.textFechaSaldo = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textFechaSaldo.setGeometry(QtCore.QRect(580, 110, 211, 31))
+        self.textFechaSaldo.setObjectName("textFechaSaldo")
+        self.deletEgreso = QtWidgets.QPushButton(self.centralwidget)
+        self.deletEgreso.setGeometry(QtCore.QRect(750, 600, 75, 23))
+        self.deletEgreso.setObjectName("deletEgreso")
+        self.deletIngreso = QtWidgets.QPushButton(self.centralwidget)
+        self.deletIngreso.setGeometry(QtCore.QRect(330, 600, 75, 23))
+        self.deletIngreso.setObjectName("deletIngreso")
+        self.editIngreso = QtWidgets.QPushButton(self.centralwidget)
+        self.editIngreso.setGeometry(QtCore.QRect(10, 600, 75, 23))
+        self.editIngreso.setObjectName("editIngreso")
+        self.editEgreso = QtWidgets.QPushButton(self.centralwidget)
+        self.editEgreso.setGeometry(QtCore.QRect(430, 600, 75, 23))
+        self.editEgreso.setObjectName("editEgreso")
         
         self.setCentralWidget(self.centralwidget)
         
@@ -91,6 +148,16 @@ class MainWindow(QtWidgets.QMainWindow):
         self.retranslateUi(self)
         QtCore.QMetaObject.connectSlotsByName(self)
 
+    def abrir_dialogo(self):
+        dialogo = MontoInputDialog()
+        if dialogo.exec_() == QDialog.Accepted:
+            monto = dialogo.monto_input.text()
+            # Actualiza el texto de self.capinicial con el monto ingresado
+            self.textCI.setPlainText(monto)
+            font = QtGui.QFont()
+            font.setPointSize(12)  # Tamaño de fuente deseado
+            self.textCI.setFont(font)
+            
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         self.setWindowTitle(_translate("MainWindow", "MainWindow"))
@@ -102,6 +169,10 @@ class MainWindow(QtWidgets.QMainWindow):
         self.cargaCI.setText(_translate("MainWindow", "Cargar"))
         self.ingreso.setText(_translate("MainWindow", "Ingreso"))
         self.egreso.setText(_translate("MainWindow", "Egreso"))
+        self.deletEgreso.setText(_translate("MainWindow", "Eliminar"))
+        self.deletIngreso.setText(_translate("MainWindow", "Eliminar"))
+        self.editIngreso.setText(_translate("MainWindow", "Editar"))
+        self.editEgreso.setText(_translate("MainWindow", "Editar"))
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
